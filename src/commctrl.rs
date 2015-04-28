8c2d87bd07a1e1b58f30fc549f8b16dc16f20478
Yonathan Randolph <yonathan@gmail.com>
2015-04-28 14:32:45 -0700

    Add some tree view structures.

--- src/commctrl.rs
+++ src/commctrl.rs
@@ -46,3 +46,118 @@ pub const CCM_GETVERSION: ::UINT = 0x2008;
 pub const CCM_SETWINDOWTHEME: ::UINT = 0x200b;
 pub const CCM_DPISCALE: ::UINT = 0x200c;  // TRUE in wParam
 
+//4794
+// Tree view styles (dwStyle param of CreateWindow/CreateWindowEx)
+pub const TVS_HASBUTTONS: ::DWORD = 0x0001;
+pub const TVS_HASLINES: ::DWORD = 0x0002;
+pub const TVS_LINESATROOT: ::DWORD = 0x0004;
+pub const TVS_EDITLABELS: ::DWORD = 0x0008;
+pub const TVS_DISABLEDRAGDROP: ::DWORD = 0x0010;
+pub const TVS_SHOWSELALWAYS: ::DWORD = 0x0020;
+pub const TVS_RTLREADING: ::DWORD = 0x0040;
+
+pub const TVS_NOTOOLTIPS: ::DWORD = 0x0080;
+pub const TVS_CHECKBOXES: ::DWORD = 0x0100;
+pub const TVS_TRACKSELECT: ::DWORD = 0x0200;
+pub const TVS_SINGLEEXPAND: ::DWORD = 0x0400;
+pub const TVS_INFOTIP: ::DWORD = 0x0800;
+pub const TVS_FULLROWSELECT: ::DWORD = 0x1000;
+pub const TVS_NOSCROLL: ::DWORD = 0x2000;
+pub const TVS_NONEVENHEIGHT: ::DWORD = 0x4000;
+pub const TVS_NOHSCROLL: ::DWORD = 0x8000;  // TVS_NOSCROLL overrides this;
+
+// Tree View extended styles (dwExStyle param of CreateWindowEx)
+// xp and above
+pub const TVS_EX_NOSINGLECOLLAPSE: ::DWORD = 0x0001;
+// vista and above:
+pub const TVS_EX_MULTISELECT: ::DWORD = 0x0002; // msdn says not supported; do not use...
+pub const TVS_EX_DOUBLEBUFFER: ::DWORD = 0x0004;
+pub const TVS_EX_NOINDENTSTATE: ::DWORD = 0x0008;
+pub const TVS_EX_RICHTOOLTIP: ::DWORD = 0x0010;
+pub const TVS_EX_AUTOHSCROLL: ::DWORD = 0x0020;
+pub const TVS_EX_FADEINOUTEXPANDOS: ::DWORD = 0x0040;
+pub const TVS_EX_PARTIALCHECKBOXES: ::DWORD = 0x0080;
+pub const TVS_EX_EXCLUSIONCHECKBOXES: ::DWORD = 0x0100;
+pub const TVS_EX_DIMMEDCHECKBOXES: ::DWORD = 0x0200;
+pub const TVS_EX_DRAWIMAGEASYNC: ::DWORD = 0x0400;
+
+struct _TREEITEM {do_not_construct: ::std::marker::PhantomData<()>}
+pub type HTREEITEM = *const _TREEITEM;
+
+//4834
+// Tree View control Item Flags (TVITEMEXW::mask)
+pub const TVIF_TEXT: ::UINT = 0x0001;
+pub const TVIF_IMAGE: ::UINT = 0x0002;
+pub const TVIF_PARAM: ::UINT = 0x0004;
+pub const TVIF_STATE: ::UINT = 0x0008;
+pub const TVIF_HANDLE: ::UINT = 0x0010;
+pub const TVIF_SELECTEDIMAGE: ::UINT = 0x0020;
+pub const TVIF_CHILDREN: ::UINT = 0x0040;
+pub const TVIF_INTEGRAL: ::UINT = 0x0080;
+// IE 6
+pub const TVIF_STATEEX: ::UINT = 0x0100;
+pub const TVIF_EXPANDEDIMAGE: ::UINT = 0x0200;
+
+// Tree View control Item States
+pub const TVIS_SELECTED: ::UINT = 0x0002;
+pub const TVIS_CUT: ::UINT = 0x0004;
+pub const TVIS_DROPHILITED: ::UINT = 0x0008;
+pub const TVIS_BOLD: ::UINT = 0x0010;
+pub const TVIS_EXPANDED: ::UINT = 0x0020;
+pub const TVIS_EXPANDEDONCE: ::UINT = 0x0040;
+pub const TVIS_EXPANDPARTIAL: ::UINT = 0x0080;
+
+pub const TVIS_OVERLAYMASK: ::UINT = 0x0F00;
+pub const TVIS_STATEIMAGEMASK: ::UINT = 0xF000;
+pub const TVIS_USERMASK: ::UINT = 0xF000;
+
+// IE 6
+pub const TVIS_EX_FLAT: ::UINT = 0x0001;
+// Vista
+pub const TVIS_EX_DISABLED: ::UINT = 0x0002;
+
+pub const TVIS_EX_ALL: ::UINT = 0x0002;
+
+#[repr(C)] #[derive(Clone, Copy, Debug)]
+pub struct TVITEMEXW {
+	pub mask: ::UINT,
+	pub hItem: HTREEITEM,
+	pub state: ::UINT,
+	pub stateMask: ::UINT,
+	pub pszText: ::LPCWSTR,
+	pub cchTextMax: ::c_int,
+	pub iImage: ::c_int,
+	pub iSelectedImage: ::c_int,
+	pub cChildren: ::c_int,
+	pub lParam: ::LPARAM,
+	pub iIntegral: ::c_int,
+	// IE 6
+	pub uStateEx: ::UINT,
+	pub hwnd: ::HWND,
+	pub iExpandedImage: ::c_int,
+	// Win7
+	pub iReserved: ::c_int,
+}
+// make it easier to construct: TVITEMEXW {<interesting fields>, ..Default::default()}
+impl ::std::default::Default for TVITEMEXW {
+	fn default () -> TVITEMEXW {
+		TVITEMEXW {mask:0, hItem: ::std::ptr::null(), state:0, stateMask:0, pszText: ::std::ptr::null_mut(), cchTextMax:0, iImage:0, iSelectedImage:0, cChildren:0, lParam:0, iIntegral:0, uStateEx:0, hwnd: ::std::ptr::null_mut(), iExpandedImage:0, iReserved:0}
+	}
+}
+
+
+pub const TVI_ROOT: HTREEITEM = -0x10000 as isize as HTREEITEM;
+pub const TVI_FIRST: HTREEITEM = -0x0FFFF as isize as HTREEITEM;
+pub const TVI_LAST: HTREEITEM = -0x0FFFE as isize as HTREEITEM;
+pub const TVI_SORT: HTREEITEM = -0x0FFFD as isize as HTREEITEM;
+
+//5000
+#[repr(C)] #[derive(Clone, Copy, Debug)]
+pub struct TVINSERTSTRUCTW {
+	pub hParent: HTREEITEM,
+	pub hInsertAfter: HTREEITEM,
+	pub itemex: TVITEMEXW,
+}
+pub const TV_FIRST: ::UINT = 0x1100;
+pub const TVM_INSERTITEMA: ::UINT = 0x1100;  // *const TVINSERTSTRUCTA in lParam
+pub const TVM_INSERTITEMW: ::UINT = (TV_FIRST + 50);  // *const TVINSERTSTRUCTW in lParam
