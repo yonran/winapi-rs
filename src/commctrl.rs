730601ccb07e55b677e99ce5cbca03be64381d59
Yonathan Randolph <yonathan@gmail.com>
2015-05-03 14:45:08 -0700

    Add a bunch more LV structs.

--- src/commctrl.rs
+++ src/commctrl.rs
@@ -23,6 +23,57 @@ pub const ICC_NATIVEFNTCTL_CLASS: ::DWORD = 0x00002000;   // native font control
 pub const ICC_STANDARD_CLASSES: ::DWORD = 0x00004000;
 pub const ICC_LINK_CLASS: ::DWORD = 0x00008000;
 
+//330
+// WM_NOTIFY codes (::c_int.code)
+pub const NM_FIRST:   ::UINT = 0; // generic to all controls
+pub const NM_LAST:    ::UINT = 0xffffff9d;  // 0-99
+pub const LVN_FIRST:  ::UINT = 0xffffff9c;  // 0-100 listview
+pub const LVN_LAST:   ::UINT = 0xffffff39;  // 0-199
+// Property sheet reserved      (0-200) -  (0-299) - see prsht.h
+pub const HDN_FIRST:  ::UINT = 0xfffffed4;  // 0-300 header
+pub const HDN_LAST:   ::UINT = 0xfffffe71;  // 0-399
+pub const TVN_FIRST:  ::UINT = 0xfffffe70;  // 0-400 treeview
+
+pub const TVN_LAST:   ::UINT = 0xfffffe0d;  // 0-499
+pub const TTN_FIRST:  ::UINT = 0xfffffdf8;  // 0-520 tooltips
+pub const TTN_LAST:   ::UINT = 0xfffffddb;  // 0-549
+pub const TCN_FIRST:  ::UINT = 0xfffffdda;  // 0-550 tab control
+pub const TCN_LAST:   ::UINT = 0xfffffdbc;  // 0-580
+// Shell reserved               (0-580) - (0-589)
+pub const CDN_FIRST:  ::UINT = 0xfffffda7;  // 0-601 common dialog (new)
+pub const CDN_LAST:   ::UINT = 0xfffffd45;  // 0-699
+pub const TBN_FIRST:  ::UINT = 0xfffffd44;  // 0-700 toolbar
+pub const TBN_LAST:   ::UINT = 0xfffffd30;  // 0-720
+pub const UDN_FIRST:  ::UINT = 0xfffffd2f;  // 0-721 updown
+pub const UDN_LAST:   ::UINT = 0xfffffd27;  // 0-729
+pub const DTN_FIRST:  ::UINT = 0xfffffd1c;  // 0-740 datetimepick
+pub const DTN_LAST:   ::UINT = 0xfffffd17;  // 0-745 DTN_FIRST - 5
+pub const MCN_FIRST:  ::UINT = 0xfffffd16;  // 0-746 monthcal
+pub const MCN_LAST:   ::UINT = 0xfffffd10;  // 0-752 MCN_FIRST - 6
+pub const DTN_FIRST2: ::UINT = 0xfffffd0f;  // 0-753 datetimepick2
+pub const DTN_LAST2:  ::UINT = 0xfffffce1;  // 0-799
+pub const CBEN_FIRST: ::UINT = 0xfffffce0;  // 0-800 combo box ex
+pub const CBEN_LAST:  ::UINT = 0xfffffcc2;  // 0-830
+pub const RBN_FIRST:  ::UINT = 0xfffffcc1;  // 0-831 rebar
+pub const RBN_LAST:   ::UINT = 0xfffffca5;  // 0-859
+pub const IPN_FIRST:  ::UINT = 0xfffffca4;  // 0-860 internet address
+pub const IPN_LAST:   ::UINT = 0xfffffc91;  // 0-879
+pub const SBN_FIRST:  ::UINT = 0xfffffc90;  // 0-880 status bar
+pub const SBN_LAST:   ::UINT = 0xfffffc7d;  // 0-899
+pub const PGN_FIRST:  ::UINT = 0xfffffc7c;  // 0-900 Pager Control
+pub const PGN_LAST:   ::UINT = 0xfffffc4a;  // 0-950
+pub const WMN_FIRST:  ::UINT = 0xfffffc18;  // 0-1000
+pub const WMN_LAST:   ::UINT = 0xfffffb50;  // 0-1200
+pub const BCN_FIRST:  ::UINT = 0xfffffb1e;  // 0-1250
+pub const BCN_LAST:   ::UINT = 0xfffffaba;  // 0-1350
+pub const TRBN_FIRST: ::UINT = 0xfffffa23;  // 0-1501 trackbar
+pub const TRBN_LAST:  ::UINT = 0xfffffa11;  // 0-1519
+pub const MSGF_COMMCTRL_BEGINDRAG: ::UINT = 0x4200;
+pub const MSGF_COMMCTRL_SIZEHEADER: ::UINT = 0x4201;
+pub const MSGF_COMMCTRL_DRAGSELECT: ::UINT = 0x4202;
+pub const MSGF_COMMCTRL_TOOLBARCUST: ::UINT = 0x4203;
+
+
 // If you enable an owner-drawn item, the DRAWITEMSTRUCT::CtlType will be one of:
 pub const ODT_HEADER: ::DWORD = 100;
 pub const ODT_TAB: ::DWORD = 101;
@@ -45,6 +96,61 @@ pub const CCM_SETVERSION: ::UINT = 0x2007;
 pub const CCM_GETVERSION: ::UINT = 0x2008;
 pub const CCM_SETWINDOWTHEME: ::UINT = 0x200b;
 pub const CCM_DPISCALE: ::UINT = 0x200c;  // TRUE in wParam
+//2989
+pub const MAX_LINKID_TEXT: usize = 48;
+pub const L_MAX_URL_LENGTH: usize = (2048 + 32 + 3 /*sizeof("://") */);
+
+//3028
+#[repr(C)] #[derive(Copy)]
+pub struct LITEM {
+    pub mask: ::UINT,
+    pub iLink: ::c_int,
+    pub state: ::UINT,
+    pub stateMask: ::UINT,
+    pub szID: [::WCHAR; MAX_LINKID_TEXT],
+    pub szUrl: [::WCHAR; L_MAX_URL_LENGTH],
+}
+pub type PLITEM = *mut LITEM;
+impl ::std::clone::Clone for LITEM {
+    fn clone(&self) -> Self {
+        let mut clone = LITEM {
+            mask: self.mask,
+            iLink: self.iLink,
+            state: self.state,
+            stateMask: self.stateMask,
+            szID: [0; MAX_LINKID_TEXT],
+            szUrl: [0; L_MAX_URL_LENGTH],
+        };
+        for i in 0..clone.szID.len() {
+            clone.szID[i] = self.szID[i];
+        }
+        for i in 0..clone.szUrl.len() {
+            clone.szUrl[i] = self.szUrl[i];
+        }
+        clone
+    }
+}
+impl ::std::fmt::Debug for LITEM {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+        try!(f.write_fmt(format_args!("LVITEM {{ mask: {}, iLink: {}, state: {}, stateMask: {}, szID: {:?}, szUrl: {:?} }}",
+            self.mask, self.iLink, self.state, self.stateMask, &self.szID[..], &self.szUrl[..])));
+        Ok(())
+    }
+}
+
+#[repr(C)] #[derive(Clone, Copy, Debug)]
+pub struct LHITTESTINFO {
+    pub pt: ::POINT,
+    pub item: LITEM,
+}
+pub type PLHITTESTINFO = *mut LHITTESTINFO;
+
+#[repr(C)] #[derive(Clone, Copy, Debug)]
+pub struct NMLINK {
+    pub hdr: ::NMHDR,
+    pub item: LITEM,
+}
+pub type PNMLINK = *mut NMLINK;
 
 //3078
 // List View Styles (dwStyle param of CreateWindow/CreateWindowEx)
@@ -209,6 +315,7 @@ impl ::std::default::Default for LVCOLUMNW {
 
 
 pub const LVM_FIRST: ::UINT = 0x1000;      // ListView messages
+pub const LVM_GETITEMCOUNT: ::UINT = (LVM_FIRST + 4);  // returns c_int
 pub const LVM_GETITEMA: ::UINT = (LVM_FIRST + 5);  // *mut LVITEMA in lParam
 pub const LVM_GETITEMW: ::UINT = (LVM_FIRST + 75);  // *mut LVITEMW in lParam
 pub const LVM_SETITEMA: ::UINT = (LVM_FIRST + 6);  // *const LVITEMA in lParam
@@ -294,6 +401,201 @@ pub const LVM_INSERTCOLUMNW: ::UINT = (LVM_FIRST + 97);  // iCol in wParam, *con
 pub const LVM_DELETECOLUMN: ::UINT = (LVM_FIRST + 28);  // iCol in wParam
 pub const LVM_GETCOLUMNWIDTH: ::UINT = (LVM_FIRST + 29);  // iCol in wParam
 
+//4433
+pub struct NMLISTVIEW {
+    pub hdr: ::NMHDR,
+    pub iItem: ::c_int,
+    pub iSubItem: ::c_int,
+    pub uNewState: ::UINT,
+    pub uOldState: ::UINT,
+    pub uChanged: ::UINT,
+    pub ptAction: ::POINT,
+    pub lParam: ::LPARAM,
+}
+pub type LPNMLISTVIEW = *mut NMLISTVIEW;
+// legacy name pub type LPNM_LISTVIEW = LPNMLISTVIEW;
+// legacy name pub type NM_LISTVIEW = NMLISTVIEW;
+
+
+//4551
+#[repr(C)] #[derive(Clone, Copy, Debug)]
+pub struct NMLVODSTATECHANGE {
+    pub hdr: ::NMHDR,
+    pub iFrom: ::c_int,
+    pub iTo: ::c_int,
+    pub uNewState: ::UINT,
+    pub uOldState: ::UINT,
+}
+pub type LPNMLVODSTATECHANGE = *mut NMLVODSTATECHANGE;
+// legacy name pub type PNM_ODSTATECHANGE = LPNMLVODSTATECHANGE;
+// legacy name pub type LPNM_ODSTATECHANGE = LPNMLVODSTATECHANGE;
+// legacy name pub type NM_ODSTATECHANGE = NMLVODSTATECHANGE;
+
+//4565
+pub const LVN_ITEMCHANGING: ::UINT = (LVN_FIRST-0);
+pub const LVN_ITEMCHANGED: ::UINT = (LVN_FIRST-1);
+pub const LVN_INSERTITEM: ::UINT = (LVN_FIRST-2);
+pub const LVN_DELETEITEM: ::UINT = (LVN_FIRST-3);
+pub const LVN_DELETEALLITEMS: ::UINT = (LVN_FIRST-4);
+pub const LVN_BEGINLABELEDITA: ::UINT = (LVN_FIRST-5);
+pub const LVN_BEGINLABELEDITW: ::UINT = (LVN_FIRST-75);
+pub const LVN_ENDLABELEDITA: ::UINT = (LVN_FIRST-6);
+pub const LVN_ENDLABELEDITW: ::UINT = (LVN_FIRST-76);
+pub const LVN_COLUMNCLICK: ::UINT = (LVN_FIRST-8);
+pub const LVN_BEGINDRAG: ::UINT = (LVN_FIRST-9);
+pub const LVN_BEGINRDRAG: ::UINT = (LVN_FIRST-11);
+
+pub const LVN_ODCACHEHINT: ::UINT = (LVN_FIRST-13);
+pub const LVN_ODFINDITEMA: ::UINT = (LVN_FIRST-52);
+pub const LVN_ODFINDITEMW: ::UINT = (LVN_FIRST-79);
+
+pub const LVN_ITEMACTIVATE: ::UINT = (LVN_FIRST-14);
+pub const LVN_ODSTATECHANGED: ::UINT = (LVN_FIRST-15);
+pub const LVN_HOTTRACK: ::UINT = (LVN_FIRST-21);
+
+pub const LVN_GETDISPINFOA: ::UINT = (LVN_FIRST-50);
+pub const LVN_GETDISPINFOW: ::UINT = (LVN_FIRST-77);
+pub const LVN_SETDISPINFOA: ::UINT = (LVN_FIRST-51);
+pub const LVN_SETDISPINFOW: ::UINT = (LVN_FIRST-78);
+// LVITEM::mask
+pub const LVIF_DI_SETITEM: ::UINT = 0x1000;
+
+//4619
+#[repr(C)] #[derive(Clone, Copy, Debug)]
+pub struct NMLVDISPINFOA {
+    pub hdr: ::NMHDR,
+    pub item: LVITEMA,
+}
+pub type LPNMLVDISPINFOA = *mut NMLVDISPINFOA;
+// legacy name pub type LV_DISPINFOA = NMLVDISPINFOA;
+
+#[repr(C)] #[derive(Clone, Copy, Debug)]
+pub struct NMLVDISPINFOW {
+    pub hdr: ::NMHDR,
+    pub item: LVITEMW,
+}
+pub type LPNMLVDISPINFOW = *mut NMLVDISPINFOW;
+// legacy name pub type LV_DISPINFOW = NMLVDISPINFOW;
+pub const LVN_KEYDOWN: ::UINT = (LVN_FIRST-55);
+
+#[repr(C, packed)] #[derive(Clone, Copy, Debug)]
+pub struct NMLVKEYDOWN {
+    pub hdr: ::NMHDR,
+    pub wVKey: ::WORD,
+    pub flags: ::UINT,
+}
+pub type LPNMLVKEYDOWN = *mut NMLVKEYDOWN;
+// legacy name pub type LV_KEYDOWN = NMLVKEYDOWN;
+
+pub const LVN_MARQUEEBEGIN: ::UINT = (LVN_FIRST-56);
+
+#[repr(C)] #[derive(Clone, Copy, Debug)]
+pub struct NMLVLINK {
+    pub hdr: ::NMHDR,
+    pub link: LITEM,
+    pub iItem: ::c_int,
+    pub iSubItem: ::c_int,
+}
+pub type PNMLVLINK = *mut NMLVLINK;
+
+#[repr(C)] #[derive(Clone, Copy, Debug)]
+pub struct NMLVGETINFOTIPA {
+
+    pub hdr: ::NMHDR,
+    pub dwFlags: ::DWORD,
+    pub pszText: ::LPSTR,
+    pub cchTextMax: ::c_int,
+    pub iItem: ::c_int,
+    pub iSubItem: ::c_int,
+    pub lParam: ::LPARAM,
+}
+pub type LPNMLVGETINFOTIPA = *mut NMLVGETINFOTIPA;
+
+#[repr(C)] #[derive(Clone, Copy, Debug)]
+pub struct NMLVGETINFOTIPW {
+
+    pub hdr: ::NMHDR,
+    pub dwFlags: ::DWORD,
+    pub pszText: ::LPWSTR,
+    pub cchTextMax: ::c_int,
+    pub iItem: ::c_int,
+    pub iSubItem: ::c_int,
+    pub lParam: ::LPARAM,
+}
+pub type LPNMLVGETINFOTIPW = *mut NMLVGETINFOTIPW;
+
+// NMLVGETINFOTIPA.dwFlag values
+pub const LVGIT_UNFOLDED: ::UINT = 0x0001;
+
+pub const LVN_GETINFOTIPA: ::UINT = (LVN_FIRST-57);
+pub const LVN_GETINFOTIPW: ::UINT = (LVN_FIRST-58);
+
+//4707
+//  LVN_INCREMENTALSEARCH gives the app the opportunity to customize
+//  incremental search.  For example, if the items are numeric,
+//  the app can do numerical search instead of string search.
+//
+//  ListView notifies the app with NMLVFINDITEM.
+//  The app sets pnmfi->lvfi.lParam to the result of the incremental search,
+//  or to LVNSCH_DEFAULT if ListView should do the default search,
+//  or to LVNSCH_ERROR to fail the search and just beep,
+//  or to LVNSCH_IGNORE to stop all ListView processing.
+//
+//  The return value is not used.
+
+pub const LVNSCH_DEFAULT: ::LPARAM = -1;
+pub const LVNSCH_ERROR: ::LPARAM = -2;
+pub const LVNSCH_IGNORE: ::LPARAM = -3;
+
+pub const LVN_INCREMENTALSEARCHA: ::UINT = (LVN_FIRST-62);
+pub const LVN_INCREMENTALSEARCHW: ::UINT = (LVN_FIRST-63);
+
+pub const LVN_COLUMNDROPDOWN: ::UINT = (LVN_FIRST-64);
+pub const LVN_COLUMNOVERFLOWCLICK: ::UINT = (LVN_FIRST-66);
+
+#[repr(C)] #[derive(Clone, Copy, Debug)]
+pub struct NMLVSCROLL {
+    pub hdr: ::NMHDR,
+    pub dx: ::c_int,
+    pub dy: ::c_int,
+}
+pub type LPNMLVSCROLL = *mut NMLVSCROLL;
+
+pub const LVN_BEGINSCROLL: ::UINT = (LVN_FIRST-80);
+pub const LVN_ENDSCROLL: ::UINT = (LVN_FIRST-81);
+
+pub const LVN_LINKCLICK: ::UINT = (LVN_FIRST-84);
+pub const EMF_CENTERED: ::UINT = 0x00000001;  // render markup centered in the listview area
+
+#[repr(C)] #[derive(Copy)]
+pub struct NMLVEMPTYMARKUP {
+    pub hdr: ::NMHDR,
+    // out params from client back to listview
+    pub dwFlags: ::DWORD,                      // EMF_*
+    pub szMarkup: [::WCHAR; L_MAX_URL_LENGTH],   // markup displayed
+}
+impl ::std::clone::Clone for NMLVEMPTYMARKUP {
+    fn clone(&self) -> Self {
+        let mut clone = NMLVEMPTYMARKUP {
+            hdr: self.hdr,
+            dwFlags: self.dwFlags,
+            szMarkup: [0; L_MAX_URL_LENGTH],
+        };
+        for i in 0..clone.szMarkup.len() {
+            clone.szMarkup[i] = self.szMarkup[i];
+        }
+        clone
+    }
+}
+impl ::std::fmt::Debug for NMLVEMPTYMARKUP {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+        try!(f.write_fmt(format_args!("NMLVEMPTYMARKUP {{ hdr: {:?}, dwFlags: {:?}, szMarkup: {:?} }}",
+            self.hdr, self.dwFlags, &self.szMarkup as &[::WCHAR])));
+        Ok(())
+    }
+}
+
+pub const LVN_GETEMPTYMARKUP: ::UINT = (LVN_FIRST-87);
 
 
 //4794
